5a52d50e0e86dd4b2e74f96a972f1abb7e0de222
gregoire.igor@gmail.com <Ms2vJpR98>
2023-11-30 12:45:10 +0100

    add SpeedVolume

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fileManager import FileExtractor
 from plotManager import PlotMaker
 
+
 f = FileExtractor("test.txt")
 p = PlotMaker("hello world", ["x", "t"], [[[0,1,2,3,4],[5,6,7,8,9]],[[5,6,7,8,9],[0,1,2,3,4]]], ["Alice", "Bob"])
-p.plotdot(2)
+
+R = 8.314
+F = 96500
+
+def faradayTheoreticalVolumeSpeed(T=298, I=1, p=1e10, z=4):
+    return (R*T*I)/(p*z*F)
+
